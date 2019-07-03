2c41a2995b310aa91fb1c3aa2841a40f554bb1fe
John Tranier <john.tranier@irit.fr>
2019-07-03 17:58:52 +0200

    Test - a user must have a firstname and a lastname

--- src/test/kotlin/org/elaastic/questions/UserRepositoryTests.kt
+++ src/test/kotlin/org/elaastic/questions/UserRepositoryTests.kt
@@ -1,12 +1,14 @@
 package org.elaastic.questions
 
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.springframework.test.context.ActiveProfiles
 import javax.transaction.Transactional
+import javax.validation.ConstraintViolationException
 
 /**
  * @author John Tranier
@@ -24,7 +26,7 @@ class UserRepositoryTests(
     }
 
     @Test
-    fun `can I create a user`() {
+    fun `save a valid user`() {
         userRepository.save(
                 User(
                         "John",
@@ -36,5 +38,20 @@ class UserRepositoryTests(
         )
 
         assertEquals(13, userRepository.findAll().count())
+    }
+
+    @Test
+    fun `a user must have a firstname and a lastname`() {
+        Assertions.assertThrows(ConstraintViolationException::class.java) { ->
+            userRepository.save(
+                    User("", "Tranier", "jtranier", "1234", "john.tranier@ticetime.com")
+            )
+        }
+
+        Assertions.assertThrows(ConstraintViolationException::class.java) { ->
+            userRepository.save(
+                    User("John", "", "jtranier", "1234", "john.tranier@ticetime.com")
+            )
+        }
     }
 }
