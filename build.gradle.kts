68b0163aa858c34e856af357a6b13d9f7f9272b9
John Tranier <john.tranier@irit.fr>
2019-07-08 22:04:40 +0200

    Set JDK 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 group = "org.elaastic.questions"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
