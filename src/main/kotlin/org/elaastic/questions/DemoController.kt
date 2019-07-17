fcf73332f429f87e05a7e859275c4282f0870c41
Franck Silvestre <franck.silvestre@gmail.com>
2019-07-17 19:32:03 +0200

    wip for spring security from thymeleaf template

--- src/main/kotlin/org/elaastic/questions/DemoController.kt
+++ src/main/kotlin/org/elaastic/questions/DemoController.kt
@@ -1,13 +1,18 @@
 package org.elaastic.questions
 
+import org.elaastic.questions.directory.Role
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class DemoController() {
 
     @GetMapping("/demo")
-    fun demo(): String {
+    fun demo(model: Model): String {
+        model.addAttribute("ADMIN_ROLE_NAME", Role.RoleId.ADMIN.name)
+        model.addAttribute("TEACHER_ROLE_NAME", Role.RoleId.TEACHER.name)
+        model.addAttribute("STUDENT_ROLE_NAME", Role.RoleId.STUDENT.name)
         return "demo"
     }
 }
