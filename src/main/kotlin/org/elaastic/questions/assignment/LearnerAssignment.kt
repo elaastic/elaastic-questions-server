ab9687a49bf9e768c81ee8249591f4f1cec2bb6b
John Tranier <john.tranier@irit.fr>
2019-07-22 12:11:11 +0200

    LearnerAssignment entity

---
+++ src/main/kotlin/org/elaastic/questions/assignment/LearnerAssignment.kt
@@ -0,0 +1,39 @@
+package org.elaastic.questions.assignment
+
+import org.elaastic.questions.directory.User
+import org.elaastic.questions.persistence.AbstractJpaPersistable
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.util.*
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+
+/**
+ * @author John Tranier
+ */
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+class LearnerAssignment(
+
+        @field:ManyToOne
+        var learner: User,
+
+        @field:OneToOne(fetch = FetchType.EAGER)
+        var assignment: Assignment
+) : AbstractJpaPersistable<Long>() {
+
+    @Version
+    var version: Long? = null
+
+    @NotNull
+    @Column(name = "date_created")
+    @CreatedDate
+    lateinit var dateCreated: Date
+
+    @NotNull
+    @LastModifiedDate
+    @Column(name = "last_updated")
+    var lastUpdated: Date? = null
+
+}
---
+++ src/test/kotlin/org/elaastic/questions/assignment/LearnerAssignmentRepositoryIntegrationTest.kt
@@ -0,0 +1,48 @@
+package org.elaastic.questions.assignment
+
+import org.elaastic.questions.test.TestingService
+import org.elaastic.questions.test.directive.tThen
+import org.elaastic.questions.test.directive.tWhen
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import javax.transaction.Transactional
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers.*
+import org.junit.jupiter.api.Test
+import javax.persistence.EntityManager
+
+
+/**
+ * @author John Tranier
+ */
+@SpringBootTest
+@Transactional
+@EnableJpaAuditing
+class LearnerAssignmentRepositoryIntegrationTest(
+        @Autowired val learnerAssignmentRepository: LearnerAssignmentRepository,
+        @Autowired val testingService: TestingService,
+        @Autowired val entityManager: EntityManager
+) {
+
+    @Test
+    fun `save a valid learner assignment`() {
+        val learner = testingService.getAnyUser()
+        val assignment = testingService.getAnyAssignment()
+        LearnerAssignment(
+                learner = learner,
+                assignment = assignment
+        ).tWhen {
+            learnerAssignmentRepository.saveAndFlush(it)
+            entityManager.refresh(it)
+            it
+        }.tThen {
+            assertThat(it.id, notNullValue())
+            assertThat(it.version, equalTo(0L))
+            assertThat(it.dateCreated, notNullValue())
+            assertThat(it.lastUpdated, notNullValue())
+            assertThat(it.learner, equalTo(learner))
+            assertThat(it.assignment, equalTo(assignment))
+        }
+    }
+}
