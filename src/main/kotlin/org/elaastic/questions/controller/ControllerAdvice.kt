f57447c9c751154e49ae3e5fef078b3ef6ecdb96
Franck Silvestre <franck.silvestre@gmail.com>
2019-09-19 15:55:35 +0200

    Handle properly the file size limit exception

--- src/main/kotlin/org/elaastic/questions/controller/ControllerAdvice.kt
+++ src/main/kotlin/org/elaastic/questions/controller/ControllerAdvice.kt
@@ -1,8 +1,16 @@
 package org.elaastic.questions.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.servlet.ModelAndView
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletRequest
+import org.springframework.web.multipart.MaxUploadSizeExceededException
+
+
 
 
 
@@ -21,6 +29,19 @@ class ControllerAdvice {
     @ModelAttribute("logoutUrl")
     fun getLogoutUrl(): String {
         return "logout"
+    }
+
+
+    @ExceptionHandler(MaxUploadSizeExceededException::class)
+    fun handleMaxSizeException(
+            exc: MaxUploadSizeExceededException,
+            request: HttpServletRequest,
+            response: HttpServletResponse): ModelAndView {
+
+        val modelAndView = ModelAndView("error")
+
+        modelAndView.model["message"] = exc.message
+        return modelAndView
     }
 
     // TODO Handle OptimisticLockingException
