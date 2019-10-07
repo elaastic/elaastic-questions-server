ed8d4cf48d4a3094d3c4510d204e014ce9ba465e
Franck Silvestre <franck.silvestre@gmail.com>
2019-10-07 14:26:40 +0200

    Add copyright and Affero GPL header in all concerned files

--- src/main/resources/db/migration/V3__TestUserWithCanBeOwnerb'1'.sql
+++ src/main/resources/db/migration/V3__TestUserWithCanBeOwnerb'1'.sql
@@ -1,2 +1,20 @@
+#
+# Elaastic - formative assessment system
+# Copyright (C) 2019. University Toulouse 1 Capitole, University Toulouse 3 Paul Sabatier
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 INSERT INTO `user` (`id`, `account_expired`, `account_locked`, `email`, `enabled`, `first_name`, `last_name`, `normalized_username`, `password`, `password_expired`, `username`, `version`, `owner_id`, `can_be_user_owner`)
 VALUES (300,0,0,'admin2@tsaap.org',1,'Admin2','Tsaap2','admin2','7110eda4d09e062aa5e4a390b0a572ac0d2c0220',0,'admin2',0,NULL,b'1');
--- src/main/resources/db/migration/V4__FillTheDataBaseWithExistingData.sql
+++ src/main/resources/db/migration/V4__FillTheDataBaseWithExistingData.sql
@@ -1,3 +1,21 @@
+#
+# Elaastic - formative assessment system
+# Copyright (C) 2019. University Toulouse 1 Capitole, University Toulouse 3 Paul Sabatier
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 INSERT INTO `user` (`id`, `account_expired`, `account_locked`, `email`, `enabled`, `first_name`, `last_name`, `normalized_username`, `password`, `password_expired`, `username`, `version`, `owner_id`, `can_be_user_owner`)
 VALUES (359,0,0,'admin3@tsaap.org',1,'Admin3','Tsaap3','admin3','7110eda4d09e062aa5e4a390b0a572ac0d2c0220',0,'admin3',0,NULL,b'1');
 
