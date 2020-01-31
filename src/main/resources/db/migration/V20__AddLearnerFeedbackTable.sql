d7f022dbed294d59c3b7e658e28fad49f7c011fb
422404 <elyanpoujol@gmail.com>
2020-01-31 10:31:47 +0100

    Create DB table: sequence_feedback (#3)

---
+++ src/main/resources/db/migration/V20__AddLearnerFeedbackTable.sql
@@ -0,0 +1,29 @@
+#
+# Elaastic - formative assessment system
+# Copyright (C) 2019. University Toulouse 1 Capitole, University Toulouse 3 Paul Sabatier
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+CREATE TABLE `sequence_feedback` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date_created` datetime NOT NULL,
+  `author_id` bigint(20) NOT NULL,
+  `sequence_id` bigint(20) NOT NULL,
+  `rating` tinyint(1) NOT NULL,
+  `explanation` text NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_sequence_feedback_author` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_sequence_feedback_sequence` FOREIGN KEY (`sequence_id`) REFERENCES `sequence` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
