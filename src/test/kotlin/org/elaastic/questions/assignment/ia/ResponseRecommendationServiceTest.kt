c4d47cc5a46df81bfab3a05eaf5bf3c088757530
John Tranier <john.tranier@ticetime.com>
2021-12-16 21:44:30 +0100

    Fix #144 Recommendation responses selection : Properly distinguish correct answers

--- src/test/kotlin/org/elaastic/questions/assignment/ia/ResponseRecommendationServiceTest.kt
+++ src/test/kotlin/org/elaastic/questions/assignment/ia/ResponseRecommendationServiceTest.kt
@@ -20,6 +20,7 @@ package org.elaastic.questions.assignment.ia
 import org.elaastic.questions.assignment.sequence.interaction.response.Response
 import org.junit.jupiter.api.Test
 import com.nhaarman.mockitokotlin2.*
+import org.elaastic.questions.assignment.sequence.interaction.ResponseId
 import org.elaastic.questions.assignment.sequence.interaction.response.ResponseRepository
 import java.math.BigDecimal
 import javax.persistence.EntityManager
@@ -27,49 +28,81 @@ import javax.persistence.EntityManager
 
 internal class ResponseRecommendationServiceTest {
 
+    companion object {
+        fun responseToString(response: Response): String {
+            val correct = response.score?.compareTo(BigDecimal(100)) == 0
+            return "#${response.id}(${correct})"
+        }
+    }
+
     @Test
     fun `Simple test of computeRecommendations`() {
         val service = ResponseRecommendationService(
-                mock<EntityManager>(),
-                mock<ResponseRepository>()
+            mock<EntityManager>(),
+            mock<ResponseRepository>()
+        )
+
+        val responseMap = mapOf<ResponseId, Response>(
+            1L to mock<Response> {
+                on { score }.doReturn(BigDecimal(100))
+                on { id }.doReturn(1)
+                on { explanation }.doReturn("Hello World, and Universe")
+            },
+            2L to mock<Response> {
+                on { score }.doReturn(BigDecimal(50))
+                on { id }.doReturn(2)
+                on { explanation }.doReturn("Hello World, and Universe")
+            },
+            3L to mock<Response> {
+                on { score }.doReturn(BigDecimal(100))
+                on { id }.doReturn(3)
+                on { explanation }.doReturn("Hello World, and Universe")
+            },
+            4L to mock<Response> {
+                on { score }.doReturn(BigDecimal(100))
+                on { id }.doReturn(4)
+                on { explanation }.doReturn("Hello World, and Universe")
+            },
+            5L to mock<Response> {
+                on { score }.doReturn(BigDecimal(50))
+                on { id }.doReturn(5)
+                on { explanation }.doReturn("Hello World, and Universe")
+            },
+            6L to mock<Response> {
+                on { score }.doReturn(BigDecimal(100))
+                on { id }.doReturn(6)
+                on { explanation }.doReturn("Hello World, and Universe")
+            },
+            7L to mock<Response> {
+                on { score }.doReturn(null)
+                on { id }.doReturn(7)
+                on { explanation }.doReturn("Hello World, and Universe")
+            },
         )
 
         service.computeRecommendations(
-                listOf(
-                        mock<Response> {
-                            on { score }.doReturn(BigDecimal(100))
-                            on { id }.doReturn(1)
-                            on { explanation }.doReturn("Hello World, and Universe")
-                        },
-                        mock<Response> {
-                            on { score }.doReturn(BigDecimal(50))
-                            on { id }.doReturn(2)
-                            on { explanation }.doReturn("Hello World, and Universe")
-                        },
-                        mock<Response> {
-                            on { score }.doReturn(BigDecimal(100))
-                            on { id }.doReturn(3)
-                            on { explanation }.doReturn("Hello World, and Universe")
-                        },
-                        mock<Response> {
-                            on { score }.doReturn(BigDecimal(100))
-                            on { id }.doReturn(4)
-                            on { explanation }.doReturn("Hello World, and Universe")
-                        },
-                        mock<Response> {
-                            on { score }.doReturn(BigDecimal(50))
-                            on { id }.doReturn(5)
-                            on { explanation }.doReturn("Hello World, and Universe")
-                        },
-                        mock<Response> {
-                            on { score }.doReturn(BigDecimal(100))
-                            on { id }.doReturn(6)
-                            on { explanation }.doReturn("Hello World, and Universe")
-                        }
-                ),
-                3
-        ).let {
-            print(it)
+            responseMap.keys.map { responseMap[it]!! },
+            3
+        ).let { explanationRecommendationMapping ->
+            val nbSelectionMap = Array<Int>(responseMap.keys.size) { 0 }
+
+            responseMap.keys.forEach { id ->
+                print(responseToString(responseMap[id]!!))
+                print(" ==> ")
+                explanationRecommendationMapping[id].map {
+                    nbSelectionMap[it.toInt() - 1]++
+                    print(responseToString(responseMap[it]!!))
+                    print(", ")
+                }
+                println()
+            }
+
+            nbSelectionMap.forEachIndexed() { i, nb ->
+                print(responseToString(responseMap[(i+1).toLong()]!!))
+                print(" ==> ")
+                println(nb)
+            }
+
         }
     }
 }
