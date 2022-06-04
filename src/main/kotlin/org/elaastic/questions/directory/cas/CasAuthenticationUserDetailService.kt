602fae510ba3deca7ad654465d842f37faa7f7ea
John Tranier <john.tranier@ticetime.com>
2022-06-04 17:47:47 +0200

    #155 Integration Central Authentication Service (CAS) - Add debug logs

--- src/main/kotlin/org/elaastic/questions/directory/cas/CasAuthenticationUserDetailService.kt
+++ src/main/kotlin/org/elaastic/questions/directory/cas/CasAuthenticationUserDetailService.kt
@@ -3,14 +3,20 @@ package org.elaastic.questions.directory.cas
 import org.springframework.security.cas.authentication.CasAssertionAuthenticationToken
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService
 import org.springframework.security.core.userdetails.UserDetails
+import java.util.logging.Logger
 
 class CasAuthenticationUserDetailService(
     private val casUserDetailService: CasUserDetailService,
     private val casKey: String,
 ) : AuthenticationUserDetailsService<CasAssertionAuthenticationToken> {
 
+    val logger: Logger = Logger.getLogger(CasAuthenticationUserDetailService::class.java.name)
 
     override fun loadUserDetails(token: CasAssertionAuthenticationToken): UserDetails {
+        logger.finest {
+            "token info: principal=${token.assertion.principal}, attributes=${token.assertion.principal.attributes}"
+        }
+
         val username: String = token.name
         return casUserDetailService.loadUserByUsername(casKey, username) ?:
         casUserDetailService.registerNewCasUser(casKey, token.assertion.principal)
