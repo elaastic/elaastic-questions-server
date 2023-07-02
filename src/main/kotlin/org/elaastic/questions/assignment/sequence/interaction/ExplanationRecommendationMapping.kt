7d93d3f3c65aa98d19327670f41a0ffca420b01a
John Tranier <john.tranier@ticetime.com>
2023-07-02 20:22:23 +0200

    Code cleaning - improve an equals method

--- src/main/kotlin/org/elaastic/questions/assignment/sequence/interaction/ExplanationRecommendationMapping.kt
+++ src/main/kotlin/org/elaastic/questions/assignment/sequence/interaction/ExplanationRecommendationMapping.kt
@@ -19,32 +19,33 @@
 package org.elaastic.questions.assignment.sequence.interaction
 
 
-
 typealias ResponseId = Long
+
 class ExplanationRecommendationMapping(val recommendationMap: Map<ResponseId, MutableList<ResponseId>>) {
 
-    constructor(responses: List<ResponseId>): this(
-            responses.associate { it to mutableListOf<ResponseId>() }
+    constructor(responses: List<ResponseId>) : this(
+        responses.associate { it to mutableListOf<ResponseId>() }
     )
 
     fun addRecommandation(forResponse: ResponseId, recommendedResponse: ResponseId) =
-            (recommendationMap[forResponse] ?: error("This responseId ($recommendedResponse) does not belong to this mapping"))
-                    .add(recommendedResponse)
+        (recommendationMap[forResponse]
+            ?: error("This responseId ($recommendedResponse) does not belong to this mapping"))
+            .add(recommendedResponse)
 
 
     fun getRecommandation(forResponse: ResponseId) =
-            recommendationMap[forResponse]
+        recommendationMap[forResponse]
 
     operator fun get(forResponse: ResponseId) =
-            getRecommandation(forResponse) ?: error("this response ($forResponse) does not belong to this mapping")
+        getRecommandation(forResponse) ?: error("this response ($forResponse) does not belong to this mapping")
 
     override fun toString() = recommendationMap.entries.map {
         "[${it.key} => ${it.value.map { it }}]\n"
     }.joinToString(", ")
 
-    override fun equals(other: Any?): Boolean {
-        return toString() == other?.toString()
-    }
+    override fun equals(other: Any?) =
+        other?.javaClass == this.javaClass && toString() == other.toString()
+
 
     override fun hashCode(): Int {
         return toString().hashCode()
