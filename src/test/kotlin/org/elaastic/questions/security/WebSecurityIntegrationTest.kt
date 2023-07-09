8ecab91aedb3d021db287ec18b690f2a2e631c1b
John Tranier <john.tranier@ticetime.com>
2023-07-09 17:18:27 +0200

    Add test case "one can access login page anonymously"

---
+++ src/test/kotlin/org/elaastic/questions/security/WebSecurityIntegrationTest.kt
@@ -0,0 +1,23 @@
+package org.elaastic.questions.security
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+internal class WebSecurityIntegrationTest(
+    @Autowired val mockMvc: MockMvc,
+) {
+
+    @Test
+    fun `one can access login page anonymously`() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/login"))
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.view().name("login"))
+    }
+}
