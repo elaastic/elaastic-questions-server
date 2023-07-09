5bfe13f64a4f2bb6cca044a922b68db4ab67b6f6
John Tranier <john.tranier@ticetime.com>
2023-07-09 17:52:17 +0200

    Add test case "test successful authentication"

--- src/test/kotlin/org/elaastic/questions/security/WebSecurityIntegrationTest.kt
+++ src/test/kotlin/org/elaastic/questions/security/WebSecurityIntegrationTest.kt
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
@@ -19,5 +23,19 @@ internal class WebSecurityIntegrationTest(
         mockMvc.perform(MockMvcRequestBuilders.get("/login"))
             .andExpect(MockMvcResultMatchers.status().isOk())
             .andExpect(MockMvcResultMatchers.view().name("login"))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun `test successful authentication`() {
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/login")
+                .with(SecurityMockMvcRequestPostProcessors.csrf())  // Add the CSRF token
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("username", "fsil")
+                .param("password", "1234")
+        )
+            .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
+            .andExpect(MockMvcResultMatchers.redirectedUrl("/home"))
     }
 }
