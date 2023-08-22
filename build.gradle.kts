0d56f7050690c4b5dcbb63d34aa03f1618b395c7
Franck Silvestre <franck.silvestre@gmail.com>
2023-08-22 15:42:25 +0200

    Reintroduce right version of spring security

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+ext["spring-security.version"]="5.8.3"
+
 plugins {
     id("org.springframework.boot") version "2.7.13"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
