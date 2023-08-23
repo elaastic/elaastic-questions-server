57eda5aa2789469bf3f29df7c8fc31fd1c50bcb2
John Tranier <tranier@gmail.com>
2023-08-23 17:15:42 +0200

    Fix assignment orders on learners home page

--- src/main/kotlin/org/elaastic/questions/assignment/LearnerAssignmentRepository.kt
+++ src/main/kotlin/org/elaastic/questions/assignment/LearnerAssignmentRepository.kt
@@ -27,7 +27,7 @@ interface LearnerAssignmentRepository : JpaRepository<LearnerAssignment, Long> {
 
     fun findByLearnerAndAssignment(learner: User, assignment: Assignment): LearnerAssignment?
 
-    @Query("select la.assignment from LearnerAssignment as la where la.learner = ?1")
+    @Query("select la.assignment from LearnerAssignment as la where la.learner = ?1 order by la.assignment.lastUpdated desc")
     fun findAllAssignmentsForLearnerWithoutPage(user: User): List<Assignment>
 
     fun countAllByAssignment(assignment: Assignment): Int
