94b925813496c7a91a1718348325c1215d8c13d3
Franck Silvestre <franck.silvestre@gmail.com>
2023-10-31 18:46:55 +0100

    fix only ten courses displayed when we want to associate a subject to a course.

--- src/main/kotlin/org/elaastic/questions/course/CourseController.kt
+++ src/main/kotlin/org/elaastic/questions/course/CourseController.kt
@@ -244,7 +244,7 @@ class CourseController(
 
         model.addAttribute("user", user)
         model.addAttribute("course", course)
-        model.addAttribute("listCourse", courseService.findAllByOwner(user).toList())
+        model.addAttribute("listCourse", courseService.findAllByOwner(user))
         model.addAttribute("subjectData", SubjectController.SubjectData(owner = user, course = course))
 
         return "subject/create"
