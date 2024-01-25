bffee8e7ae87463eb33bed12d3148f14954fc8d3
RemiSaurel <r.saurel5@gmail.com>
2024-01-25 15:02:51 +0100

    feat(favourite): fix migration script + add favourite

--- src/main/resources/db/migration/V54__Add_favourite_column.sql
+++ src/main/resources/db/migration/V54__Add_favourite_column.sql
@@ -1,15 +1,27 @@
+-- Add the is_favourite column
 ALTER TABLE `choice_interaction_response`
     ADD COLUMN `is_favourite` bool NOT NULL DEFAULT FALSE;
 
+-- Get the CORRECT (score = 100) responses from a STUDENT
 WITH RankedResponses AS (
     SELECT
         id,
         interaction_id,
         mean_grade,
+        learner_id,
         ROW_NUMBER() OVER (PARTITION BY interaction_id ORDER BY mean_grade DESC) AS row_num
     FROM choice_interaction_response
+    WHERE learner_id IN (
+        SELECT u.id
+        FROM user u
+                 JOIN user_role ur ON u.id = ur.user_id
+                 JOIN role ro ON ur.role_id = ro.id
+        WHERE ro.authority = 'STUDENT_ROLE'
+    )
+      AND score = 100
 )
 
+-- Update the TOP 3 responses to be favourites
 UPDATE choice_interaction_response c
     JOIN RankedResponses r ON c.id = r.id
-    SET c.is_favourite = (r.row_num <= 3);
+    SET is_favourite = (r.row_num <= 3);
