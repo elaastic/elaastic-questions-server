7fd31f316bb2b9352ac3ee1522664e8edd5ff407
RemiSaurel <r.saurel5@gmail.com>
2024-02-01 12:38:32 +0100

    fix(db): migration script

--- src/main/resources/db/migration/V54__Add_recommended_column.sql
+++ src/main/resources/db/migration/V54__Add_recommended_column.sql
@@ -1,8 +1,3 @@
--- DROP IF EXISTS the columns
-ALTER TABLE `choice_interaction_response`
-    DROP COLUMN `is_recommended_by_system`,
-    DROP COLUMN `is_recommended_by_teacher`;
-
 -- Add the is_recommended_by_teacher column
 ALTER TABLE `choice_interaction_response`
     ADD COLUMN `is_recommended_by_teacher` bool NOT NULL DEFAULT FALSE;
