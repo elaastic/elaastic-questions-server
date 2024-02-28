c0c2c5b5d4f389ac15b1b5e049ae5b97f5627d00
denis <denis.ollivier@irit.fr>
2024-02-28 07:58:29 +0100

    feature (script-demo) : creation of script filling data for demonstration, importable with Konsolidation

---
+++ src/main/kotlin/org/elaastic/questions/bootstrap/DemoBootstrap.kt
@@ -0,0 +1,43 @@
+/*
+ * Elaastic - formative assessment system
+ * Copyright (C) 2019. University Toulouse 1 Capitole, University Toulouse 3 Paul Sabatier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.elaastic.questions.bootstrap
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Profile
+import org.springframework.stereotype.Component
+import java.util.logging.Logger
+import javax.annotation.PostConstruct
+
+
+@Component
+@Profile("demo")
+class DemoBootstrap(
+        @Autowired val bootstrapService: BootstrapService
+) {
+
+    val LOG : Logger = Logger.getLogger(DemoBootstrap::class.toString())
+
+    @PostConstruct
+    fun init() {
+        LOG.info("Bootstrapping elaastic-questions in demo and testing mode")
+        bootstrapService.initializeDemoContent()
+        LOG.info("End of the bootstrap")
+    }
+
+}
