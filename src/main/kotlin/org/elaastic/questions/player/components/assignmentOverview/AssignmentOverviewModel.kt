af4210ad6997476bb2d0b973316bb1721dde355f
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-16 14:44:38 +0200

    feat(Assignment): add attendees attribute
    to AssignmentOverviewModel data class

--- src/main/kotlin/org/elaastic/questions/player/components/assignmentOverview/AssignmentOverviewModel.kt
+++ src/main/kotlin/org/elaastic/questions/player/components/assignmentOverview/AssignmentOverviewModel.kt
@@ -25,6 +25,7 @@ data class AssignmentOverviewModel(
         val nbRegisteredUser: Int,
         val attendees: List<LearnerAssignment>,
         val openedPane: String,
+
         val assignmentTitle: String,
         val courseTitle: String?,
         val courseId: Long?,
