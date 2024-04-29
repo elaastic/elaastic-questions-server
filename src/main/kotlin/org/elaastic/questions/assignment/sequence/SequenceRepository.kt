ff56e0c58fd9f00251bd31a85cebe505886c30df
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-04-29 16:34:59 +0200

    refactor(Sequence): add pageable definition on previous and next methods
    on repository

--- src/main/kotlin/org/elaastic/questions/assignment/sequence/SequenceRepository.kt
+++ src/main/kotlin/org/elaastic/questions/assignment/sequence/SequenceRepository.kt
@@ -19,7 +19,9 @@
 package org.elaastic.questions.assignment.sequence
 
 import org.elaastic.questions.assignment.Assignment
+import org.elaastic.questions.assignment.sequence.interaction.response.Response
 import org.elaastic.questions.subject.statement.Statement
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -50,12 +52,14 @@ interface SequenceRepository : JpaRepository<Sequence, Long> {
            "AND s.rank < :rank " +
            "ORDER BY s.rank DESC")
     fun findPreviousSequence(@Param("rank") rank: Int,
-                             @Param("assignment") assignment: Assignment): Sequence?
+                             @Param("assignment") assignment: Assignment,
+                             pageable: Pageable): List<Sequence>
 
     @Query("SELECT s FROM Sequence s " +
            "WHERE s.assignment = :assignment " +
            "AND s.rank > :rank " +
            "ORDER BY s.rank")
     fun findNextSequence(@Param("rank") rank: Int,
-                         @Param("assignment") assignment: Assignment): Sequence?
+                         @Param("assignment") assignment: Assignment,
+                         pageable: Pageable): List<Sequence>
 }
