233ae8051d185790d98487284d26b47987ed8451
François SP <francois-sp@gmx.fr>
2024-05-02 13:46:15 +0200

    feat(documentation): add KDoc to the `Sequence` class

--- src/main/kotlin/org/elaastic/questions/assignment/ExecutionContext.kt
+++ src/main/kotlin/org/elaastic/questions/assignment/ExecutionContext.kt
@@ -18,8 +18,33 @@
 
 package org.elaastic.questions.assignment
 
+/**
+ * Enumeration of the different execution contexts for an assignment.
+ *
+ * @property FaceToFace The assignment is done face to face.
+ * @property Distance The assignment is done at a distance context.
+ * @property Blended The assignment is done in a blended context.
+ */
 enum class ExecutionContext {
+    /**
+     * The assignment is done face to face.
+     * That means the student must wait until the teacher opens the next phase.
+     * The teacher controls every step of the assignment.
+     */
     FaceToFace,
+
+    /**
+     * The assignment is done at a distance context.
+     * That means each student can complete the sequence at their own pace and directly see the result.
+     * The teacher can only control the opening and closing of the sequence.
+     */
     Distance,
+
+    /**
+     * The assignment is done in a blended context.
+     * That means the student can complete the two first phases at their own pace.
+     * But the reveal of the result is controlled by the teacher.
+     * The teacher can control the opening, closing of the sequence and the publication of the result.
+     */
     Blended
 }
--- src/main/kotlin/org/elaastic/questions/player/phase/evaluation/EvaluationPhaseConfig.kt
+++ src/main/kotlin/org/elaastic/questions/player/phase/evaluation/EvaluationPhaseConfig.kt
@@ -2,10 +2,17 @@ package org.elaastic.questions.player.phase.evaluation
 
 import org.elaastic.questions.player.phase.descriptor.PhaseConfig
 
-enum class EvaluationPhaseConfig: PhaseConfig {
-    // Implementation that shows all the alternative peer answers to evaluate
+/**
+ * Enumeration of the different configuration for the evaluation phase.
+ *
+ * @property ALL_AT_ONCE Implementation that shows all the alternative peer
+ *     answers to evaluate
+ * @property DRAXO ONE_BY_ONE with DRAXO Form evaluation
+ */
+enum class EvaluationPhaseConfig : PhaseConfig {
+    /** Implementation that shows all the alternative peer answers to evaluate */
     ALL_AT_ONCE,
 
-    // ONE_BY_ONE with DRAXO Form evaluation
+    /** ONE_BY_ONE with DRAXO Form evaluation */
     DRAXO;
 }
