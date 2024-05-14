450229622e2ca4bb8f4ab2b877841593c052dcd8
François SP <francois-sp@gmx.fr>
2024-05-14 16:55:50 +0200

    test(dashboard-countReport): add test

--- src/test/kotlin/org/elaastic/questions/assignment/sequence/LearnerSequenceServiceTest.kt
+++ src/test/kotlin/org/elaastic/questions/assignment/sequence/LearnerSequenceServiceTest.kt
@@ -1,13 +1,96 @@
 package org.elaastic.questions.assignment.sequence
 
+import org.elaastic.questions.assignment.AssignmentService
+import org.elaastic.questions.assignment.sequence.interaction.response.Response
+import org.elaastic.questions.assignment.sequence.interaction.response.ResponseRepository
+import org.elaastic.questions.assignment.sequence.interaction.response.ResponseService
+import org.elaastic.questions.assignment.sequence.peergrading.PeerGradingRepository
+import org.elaastic.questions.assignment.sequence.peergrading.PeerGradingService
+import org.elaastic.questions.assignment.sequence.peergrading.draxo.DraxoEvaluation
+import org.elaastic.questions.assignment.sequence.peergrading.draxo.DraxoPeerGrading
+import org.elaastic.questions.assignment.sequence.peergrading.draxo.criteria.Criteria
+import org.elaastic.questions.assignment.sequence.peergrading.draxo.option.OptionId
+import org.elaastic.questions.directory.UserService
+import org.elaastic.questions.subject.SubjectService
+import org.elaastic.questions.subject.statement.StatementService
+import org.elaastic.questions.test.IntegrationTestingService
+import org.elaastic.questions.test.directive.tGiven
+import org.elaastic.questions.test.directive.tThen
+import org.elaastic.questions.test.directive.tWhen
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import javax.persistence.EntityManager
+import javax.transaction.Transactional
 
-class LearnerSequenceServiceTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Transactional
+internal class LearnerSequenceServiceTest(
+    @Autowired val integrationTestingService: IntegrationTestingService,
+    @Autowired val responseService: ResponseService,
+    @Autowired val responseRepository: ResponseRepository,
+    @Autowired val sequenceRepository: SequenceRepository,
+    @Autowired val sequenceService: SequenceService,
+    @Autowired val assignmentService: AssignmentService,
+    @Autowired val statementService: StatementService,
+    @Autowired val userService: UserService,
+    @Autowired val peerGradingRepository: PeerGradingRepository,
+    @Autowired val entityManager: EntityManager,
+    @Autowired val subjectService: SubjectService,
+    @Autowired val peerGradingService: PeerGradingService,
+    @Autowired val learnerSequenceRepository: LearnerSequenceRepository
+) {
+
+    @Autowired
+    private lateinit var learnerSequenceService: LearnerSequenceService
 
     @Test
     fun countReportMade() {
-        TODO("Not yet implemented")
+        // Given
+        val response = Response(
+            integrationTestingService.getTestStudent(),
+            integrationTestingService.getAnyInteraction(),
+            1,
+            null,
+            null,
+            null,
+            null,
+            null,
+            false,
+            0,
+            0,
+            integrationTestingService.getAnyStatement(),
+            hiddenByTeacher = false,
+            recommendedByTeacher = false
+        )
+        responseRepository.save(response)
+        val sequence = response.interaction.sequence
+        val reporter = response.learner
+        val grader = integrationTestingService.getNLearners(1).first()
+
+        assertEquals(0, response.evaluationCount)
+
+        tGiven("A peerGrading") {
+            DraxoPeerGrading(
+                grader = grader,
+                response = response,
+                draxoEvaluation = DraxoEvaluation().addEvaluation(Criteria.D, OptionId.NO),
+                lastSequencePeerGrading = false
+            )
+                .tWhen {
+                    peerGradingRepository.save(it)
+                    it
+                }
+        }.tThen("The report count should be 0") { peerGrading ->
+            assertEquals(0, learnerSequenceService.countReportMade(reporter, sequence))
+            peerGrading
+        }.tWhen ("The reporter report it") { peerGrading ->
+            peerGradingService.updateReport(reporter, peerGrading, listOf("reason"))
+        }.tThen("The report count should be 1") {
+            assertEquals(1, learnerSequenceService.countReportMade(reporter, sequence))
+        }
+
     }
 }
