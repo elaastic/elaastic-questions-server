855a5dcafadd58d938d107f23f7b33b4e18a9734
François SP <francois-sp@gmx.fr>
2024-05-23 14:02:02 +0200

    fix: Add equals and hashCode methods to ChatGptEvaluationModel
    To remove warning

--- src/main/kotlin/org/elaastic/questions/player/components/evaluation/chatGptEvaluation/ChatGptEvaluationModel.kt
+++ src/main/kotlin/org/elaastic/questions/player/components/evaluation/chatGptEvaluation/ChatGptEvaluationModel.kt
@@ -16,3 +16,18 @@ data class ChatGptEvaluationModel(
     val reportValues: Array<ReportReason> = ReportReason.values(),
     var viewedByTeacher: Boolean = false,
 )
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ChatGptEvaluationModel
+
+        return evaluationId == other.evaluationId
+    }
+
+    override fun hashCode(): Int {
+        return evaluationId?.hashCode() ?: 0
+    }
+
+}
