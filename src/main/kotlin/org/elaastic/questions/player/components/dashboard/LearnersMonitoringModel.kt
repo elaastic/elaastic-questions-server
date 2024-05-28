1dfcc462ba5284f2d94f266919993a5b2ff1e986
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-05-28 15:37:10 +0200

    feat(Attendees): add `getStateCell()` method and private ones
    and create `StateCell` enum class

--- src/main/kotlin/org/elaastic/questions/player/components/dashboard/LearnersMonitoringModel.kt
+++ src/main/kotlin/org/elaastic/questions/player/components/dashboard/LearnersMonitoringModel.kt
@@ -1,5 +1,7 @@
 package org.elaastic.questions.player.components.dashboard;
 
+import org.elaastic.questions.player.phase.LearnerPhaseType
+
 /**
  * Model for the learners monitoring dashboard.
  *
@@ -12,7 +14,7 @@ class LearnersMonitoringModel(
     val phase1State: PhaseState,
     val phase2State: PhaseState,
     val phase3State: PhaseState,
-    val learners: List<LearnerMonitoringModel>
+    val learners: MutableList<LearnerMonitoringModel>
 )
 
 /**
@@ -31,8 +33,65 @@ class LearnerMonitoringModel(
     val learnerName: String,
     val learnerStateOnPhase1: LearnerStateOnPhase,
     val learnerStateOnPhase2: LearnerStateOnPhase,
-    val learnerStateOnPhase3: LearnerStateOnPhase
-)
+    val learnerStateOnPhase3: LearnerStateOnPhase,
+    val learnersMonitoringModel: LearnersMonitoringModel) {
+
+    /**
+     * Return the StateCell from a given phase
+     * @property phase
+     */
+    fun getStateCell(phase: LearnerPhaseType): StateCell
+        = when (this.getLearnerPhaseStateByType(phase)) {
+            LearnerStateOnPhase.ACTIVITY_NOT_TERMINATED -> {
+                if (this.getPhaseStateByType(phase) == PhaseState.IN_PROGRESS) {
+                    StateCell.IN_PROGRESS
+                } else {
+                    StateCell.NOT_TERMINATED
+                }
+            }
+            LearnerStateOnPhase.ACTIVITY_TERMINATED -> {
+                if (this.getPhaseStateByType(phase) == PhaseState.COMPLETED) {
+                    StateCell.TERMINATED
+                } else {
+                    StateCell.NOT_TERMINATED
+                }
+            }
+            LearnerStateOnPhase.WAITING -> StateCell.LOCKED
+        }
+
+    /**
+     * Return the LearnerStateOnPhase from the type of the phase
+     * @property phase The type of the phase
+     * @see LearnerPhaseType
+     */
+    private fun getLearnerPhaseStateByType(phase: LearnerPhaseType): LearnerStateOnPhase {
+        return when (phase) {
+            LearnerPhaseType.RESPONSE   -> this.learnerStateOnPhase1
+            LearnerPhaseType.EVALUATION -> this.learnerStateOnPhase2
+            LearnerPhaseType.RESULT     -> this.learnerStateOnPhase3
+        }
+    }
+
+    /**
+     * Return the PhaseState from the type of the phase
+     * @property phase The type of the phase
+     * @see LearnerPhaseType
+     */
+    private fun getPhaseStateByType(phase: LearnerPhaseType): PhaseState {
+        return when (phase) {
+            LearnerPhaseType.RESPONSE   -> this.learnersMonitoringModel.phase1State
+            LearnerPhaseType.EVALUATION -> this.learnersMonitoringModel.phase2State
+            LearnerPhaseType.RESULT     -> this.learnersMonitoringModel.phase3State
+        }
+    }
+
+    enum class StateCell {
+        LOCKED,
+        IN_PROGRESS,
+        NOT_TERMINATED,
+        TERMINATED
+    }
+}
 
 /**
  * Enum defining states of a phase
