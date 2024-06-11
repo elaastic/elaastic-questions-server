e7c14a3b26f7aae26bbe71c4c44ee896d195e945
François SP <francois-sp@gmx.fr>
2024-06-11 11:37:00 +0200

    style(fix-249): reformat file

--- src/main/kotlin/org/elaastic/questions/assignment/sequence/interaction/chatGptEvaluation/ChatGptEvaluationService.kt
+++ src/main/kotlin/org/elaastic/questions/assignment/sequence/interaction/chatGptEvaluation/ChatGptEvaluationService.kt
@@ -29,15 +29,19 @@ class ChatGptEvaluationService(
 
     @Async
     @Transactional(propagation = Propagation.NEVER)
-    fun createEvaluation(response: Response, language: String, chatGptExistingEvaluation : ChatGptEvaluation? = null): ChatGptEvaluation {
+    fun createEvaluation(
+        response: Response,
+        language: String,
+        chatGptExistingEvaluation: ChatGptEvaluation? = null
+    ): ChatGptEvaluation {
 
         val title = response.statement.title
         val questionContent = response.statement.content
         val teacherExplanation = response.statement.expectedExplanation
         val studentExplanation = response.explanation
 
-        requireNotNull(teacherExplanation){ throw IllegalArgumentException("Error: You must define an expected explanation to create a ChatGPT evaluation") }
-        requireNotNull(studentExplanation){ throw IllegalArgumentException("Error: No explanation to evaluate") }
+        requireNotNull(teacherExplanation) { throw IllegalArgumentException("Error: You must define an expected explanation to create a ChatGPT evaluation") }
+        requireNotNull(studentExplanation) { throw IllegalArgumentException("Error: No explanation to evaluate") }
 
         val chatGptEvaluation = chatGptExistingEvaluation ?: ChatGptEvaluation(response = response)
         markEvaluationAsPending(chatGptEvaluation)
@@ -101,74 +105,102 @@ class ChatGptEvaluationService(
     }
 
     /**
-     *  Update the utility grade associated with a chatGPT evaluation.
+     * Update the utility grade associated with a chatGPT evaluation.
      *
-     *  @param chatGptEvaluation the chatGPT evaluation to update.
-     *  @param utilityGrade the utility grade.
+     * @param chatGptEvaluation the chatGPT evaluation to update.
+     * @param utilityGrade the utility grade.
      */
     fun changeUtilityGrade(chatGptEvaluation: ChatGptEvaluation, utilityGrade: UtilityGrade) {
         reportCandidateService.updateGrade(chatGptEvaluation, utilityGrade, chatGptEvaluationRepository)
     }
 
     /**
-     *  Update the report associated with a chatGPT evaluation.
+     * Update the report associated with a chatGPT evaluation.
      *
-     *  @param chatGptEvaluation the chatGPT evaluation to update.
-     *  @param reportReasons the reasons for the report.
-     *  @param reportComment the comment for the report.
+     * @param chatGptEvaluation the chatGPT evaluation to update.
+     * @param reportReasons the reasons for the report.
+     * @param reportComment the comment for the report.
      */
-    fun reportEvaluation(chatGptEvaluation: ChatGptEvaluation, reportReasons: List<String>, reportComment : String? = null) {
-        reportCandidateService.updateReport(chatGptEvaluation, reportReasons, reportComment, chatGptEvaluationRepository)
+    fun reportEvaluation(
+        chatGptEvaluation: ChatGptEvaluation,
+        reportReasons: List<String>,
+        reportComment: String? = null
+    ) {
+        reportCandidateService.updateReport(
+            chatGptEvaluation,
+            reportReasons,
+            reportComment,
+            chatGptEvaluationRepository
+        )
     }
 
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    fun markEvaluationAsPending(chatGptEvaluation: ChatGptEvaluation) : ChatGptEvaluation {
+    fun markEvaluationAsPending(chatGptEvaluation: ChatGptEvaluation): ChatGptEvaluation {
         chatGptEvaluation.status = ChatGptEvaluationStatus.PENDING.name
         return chatGptEvaluationRepository.saveAndFlush(chatGptEvaluation)
     }
 
     /**
-     * Check if the visibility of a chatGPT evaluation can be changed by the given user
+     * Check if the visibility of a chatGPT evaluation can be changed by the
+     * given user
      *
-     * A user can hide a chatGPT evaluation if the user is the teacher of the sequence and if the evaluation is done.
+     * A user can hide a chatGPT evaluation if the user is the teacher of the
+     * sequence and if the evaluation is done.
      *
      * @param chatGptEvaluation the chatGPT evaluation to check.
-     * @param user the user who wants to update the visibility of the evaluation.
-     * @return true if the visibility of the chatGPT evaluation can be changed, false otherwise.
+     * @param user the user who wants to update the visibility of the
+     *     evaluation.
+     * @return true if the visibility of the chatGPT evaluation can be changed,
+     *     false otherwise.
      */
     fun canUpdateVisibilityEvaluation(chatGptEvaluation: ChatGptEvaluation, user: User): Boolean {
-        return responseService.canHidePeerGrading(user, chatGptEvaluation.response) && chatGptEvaluation.status == ChatGptEvaluationStatus.DONE.name
+        return responseService.canHidePeerGrading(
+            user,
+            chatGptEvaluation.response
+        ) && chatGptEvaluation.status == ChatGptEvaluationStatus.DONE.name
     }
 
     /**
      * Hide a chatGPT evaluation.
      *
-     * An user must have the permission to hide the evaluation.
-     * If the user doesn't have the permission, an exception is thrown.
+     * An user must have the permission to hide the evaluation. If the user
+     * doesn't have the permission, an exception is thrown.
      *
      * @param chatGptEvaluation the chatGPT evaluation to hide.
      * @param user the user who wants to hide the evaluation.
-     * @throws IllegalAccessException if the user doesn't have the permission to hide the evaluation.
+     * @throws IllegalAccessException if the user doesn't have the permission
+     *     to hide the evaluation.
      */
     @Throws(IllegalAccessException::class)
     fun markAsHidden(chatGptEvaluation: ChatGptEvaluation, user: User) {
-        requireAccess(canUpdateVisibilityEvaluation(chatGptEvaluation, user)) {"You don't have the permission to hide this evaluation"}
+        requireAccess(
+            canUpdateVisibilityEvaluation(
+                chatGptEvaluation,
+                user
+            )
+        ) { "You don't have the permission to hide this evaluation" }
         reportCandidateService.markAsHidden(chatGptEvaluation, chatGptEvaluationRepository)
     }
 
     /**
      * Unhide a chatGPT evaluation.
      *
-     * An user must have the permission to unhide the evaluation.
-     * If the user doesn't have the permission, an exception is thrown.
+     * An user must have the permission to unhide the evaluation. If the user
+     * doesn't have the permission, an exception is thrown.
      *
-     * @param chatGptEvaluation the chatGPT evaluation to unhide
+     * @param chatGPTEvaluation the chatGPT evaluation to unhide
      * @param user the user who wants to unhide the evaluation.
-     * @throws IllegalAccessException if the user doesn't have the permission to unhide the evaluation.
+     * @throws IllegalAccessException if the user doesn't have the permission
+     *     to unhide the evaluation.
      */
     @Throws(IllegalAccessException::class)
     fun markAsShown(chatGPTEvaluation: ChatGptEvaluation, user: User) {
-        requireAccess(canUpdateVisibilityEvaluation(chatGPTEvaluation, user)) {"You don't have the permission to unhide this evaluation"}
+        requireAccess(
+            canUpdateVisibilityEvaluation(
+                chatGPTEvaluation,
+                user
+            )
+        ) { "You don't have the permission to unhide this evaluation" }
         reportCandidateService.markAsShown(chatGPTEvaluation, chatGptEvaluationRepository)
     }
 }
