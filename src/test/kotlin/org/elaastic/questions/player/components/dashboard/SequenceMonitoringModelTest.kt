9c2a1259ee8bf53ed5aed8eea6ca3422892f352d
François SP <francois-sp@gmx.fr>
2024-06-11 10:17:40 +0200

    refactor(Dashboard): change name of the test class to match the tested class's name

--- src/test/kotlin/org/elaastic/questions/player/components/dashboard/SequenceMonitoringModelTest.kt
+++ src/test/kotlin/org/elaastic/questions/player/components/dashboard/SequenceMonitoringModelTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 
-class LearnersMonitoringModelTest(
+class SequenceMonitoringModelTest(
 
 ) {
     private val namesUnsorted: List<String> = mutableListOf(
