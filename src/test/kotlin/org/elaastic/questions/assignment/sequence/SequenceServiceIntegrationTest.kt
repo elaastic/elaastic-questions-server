8e87f2f888dbb715b374606a507847fbd1d25a26
François SP <francois-sp@gmx.fr>
2024-06-12 16:23:56 +0200

    test(detail-answer-student): add test to the `SequenceService.findById` function

--- src/test/kotlin/org/elaastic/questions/assignment/sequence/SequenceServiceIntegrationTest.kt
+++ src/test/kotlin/org/elaastic/questions/assignment/sequence/SequenceServiceIntegrationTest.kt
@@ -20,12 +20,16 @@ package org.elaastic.questions.assignment.sequence
 
 import org.elaastic.questions.assignment.Assignment
 import org.elaastic.questions.test.IntegrationTestingService
+import org.elaastic.questions.test.directive.tGiven
+import org.elaastic.questions.test.directive.tThen
+import org.elaastic.questions.test.directive.tWhen
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import javax.transaction.Transactional
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.CoreMatchers.*
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.assertThrows
 import org.springframework.security.access.AccessDeniedException
 import javax.persistence.EntityNotFoundException
@@ -34,8 +38,8 @@ import javax.persistence.EntityNotFoundException
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 internal class SequenceServiceIntegrationTest(
-        @Autowired val sequenceService: SequenceService,
-        @Autowired val integrationTestingService: IntegrationTestingService
+    @Autowired val sequenceService: SequenceService,
+    @Autowired val integrationTestingService: IntegrationTestingService
 ) {
 
     @Test
@@ -44,15 +48,15 @@ internal class SequenceServiceIntegrationTest(
         val user = assignment.owner
 
         assertThat(
-                "The testing data are corrupted",
-                assignment.sequences.size,
-                equalTo(2)
+            "The testing data are corrupted",
+            assignment.sequences.size,
+            equalTo(2)
         )
 
         val testingSequence = assignment.sequences.first()
         assertThat(
-                sequenceService.get(user, testingSequence.id!!),
-                equalTo(testingSequence)
+            sequenceService.get(user, testingSequence.id!!),
+            equalTo(testingSequence)
         )
     }
 
@@ -62,9 +66,9 @@ internal class SequenceServiceIntegrationTest(
         val user = integrationTestingService.getTestTeacher()
 
         assertThat(
-                "The testing data are corrupted",
-                assignment.sequences.size,
-                equalTo(2)
+            "The testing data are corrupted",
+            assignment.sequences.size,
+            equalTo(2)
         )
 
         val testingSequence = assignment.sequences.first()
@@ -80,5 +84,56 @@ internal class SequenceServiceIntegrationTest(
         assertThrows<EntityNotFoundException> {
             sequenceService.get(user, 12345678)
         }
+    }
+
+    @Test
+    fun `find sequence by rank and assignment - valid`() {
+        val assignment: Assignment = integrationTestingService.getTestAssignment()
+        val sequence = assignment.sequences[0]
+        val rank: Int = sequence.rank
+
+        assertThat(
+            sequenceService.findSequenceByRankAndAssignment(rank, assignment),
+            equalTo(sequence)
+        )
+    }
+
+    @Test
+    fun `find sequence by rank and assignment - with some with same rank`() {  // TODO
+        val assignment: Assignment = integrationTestingService.getTestAssignment()
+        val sequence = assignment.sequences[1]
+        val rank: Int = sequence.rank
+
+        assertThat(
+            sequenceService.findSequenceByRankAndAssignment(rank, assignment),
+            equalTo(sequence)
+        )
+    }
+
+    @Test
+    fun `test of findById`() {
+        lateinit var sequence: Sequence
+
+        tGiven("a sequence") {
+            sequence = integrationTestingService.getAnySequence()
+        }.tWhen("we try to find it by id") {
+            val block: () -> Sequence = { sequenceService.findById(sequence.id!!) }
+            block
+        }.tThen("we find the sequence") {
+            val sequenceFound = sequenceService.findById(sequence.id!!)
+            assertNotNull(sequenceFound)
+            assertEquals(sequence, sequenceFound)
+        }
+
+        tGiven("a fake id") {
+            798546L
+        }.tWhen("we find the sequence") {
+            { sequenceService.findById(it) }
+        }.tThen {
+            assertThrows(EntityNotFoundException::class.java) {
+                it()
+            }
+        }
+
     }
 }
