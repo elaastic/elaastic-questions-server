52bff3e25ea2790f89d864a84b0aec7939a3e997
François SP <francois-sp@gmx.fr>
2024-06-12 15:35:20 +0200

    test(fix-249): add test to the `AccessUtil` class

---
+++ src/test/kotlin/org/elaastic/questions/test/util/AccessUtilTest.kt
@@ -0,0 +1,32 @@
+package org.elaastic.questions.test.util
+
+import org.elaastic.questions.util.requireAccess
+import org.elaastic.questions.util.requireAccessThrowDenied
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.*
+import org.springframework.security.access.AccessDeniedException as AccessDeniedExceptionSpring
+
+class AccessUtilTest {
+
+    @Test
+    fun `test of requireAccess`() {
+        assertDoesNotThrow {
+            requireAccess(true) { "This should not throw an exception" }
+        }
+        assertThrows(IllegalAccessException::class.java) {
+            requireAccess(false) { "This should throw an exception" }
+        }
+    }
+
+    @Test
+    fun `test of requireAccessThrowDenied`() {
+        assertDoesNotThrow {
+            requireAccessThrowDenied(true) { "This should not throw an exception" }
+        }
+        // I rename the import to avoid confusion between the two classes (one is from kotlin, the other from spring)
+        assertThrows(AccessDeniedExceptionSpring::class.java) {
+            requireAccessThrowDenied(false) { "This should throw an exception" }
+        }
+    }
+}
