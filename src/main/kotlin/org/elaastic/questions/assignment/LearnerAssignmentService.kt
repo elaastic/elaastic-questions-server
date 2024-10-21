b00d48a67121b6f5bacb46e7e61863f0e2621489
François SP <francois-sp@gmx.fr>
2024-10-21 10:50:17 +0200

    chore(report-manager): Extract the `isGraderRegisteredOnAssignment` method to a more relevant Service

--- src/main/kotlin/org/elaastic/questions/assignment/LearnerAssignmentService.kt
+++ src/main/kotlin/org/elaastic/questions/assignment/LearnerAssignmentService.kt
@@ -1,5 +1,6 @@
 package org.elaastic.questions.assignment
 
+import org.elaastic.questions.assignment.sequence.interaction.response.Response
 import org.elaastic.questions.directory.User
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -13,4 +14,10 @@ class LearnerAssignmentService(
 
     fun isRegistered(learner: User, assignment: Assignment): Boolean =
             learnerAssignmentRepository.findByLearnerAndAssignment(learner, assignment) != null
+
+    fun isGraderRegisteredOnAssignment(grader: User, response: Response) =
+        isRegistered(
+            grader,
+            response.interaction.sequence.assignment ?: error("The response is not bound to an assignment")
+        )
 }
