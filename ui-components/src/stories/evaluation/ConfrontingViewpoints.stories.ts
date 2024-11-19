74185feef7c7717a20ddc26fffea6264615278b1
John Tranier <tranier@gmail.com>
2024-11-19 09:34:46 +0100

    Fix ui-components build

--- ui-components/src/stories/evaluation/ConfrontingViewpoints.stories.ts
+++ ui-components/src/stories/evaluation/ConfrontingViewpoints.stories.ts
@@ -1,12 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/vue3'
 
 import ConfrontingViewpoint from '@/stories/evaluation/ConfrontingViewpoint.vue'
+import type { ExclusiveChoiceResponse, MultipleChoiceResponse, OpenEndedResponse } from '@/models/Response'
 
 const meta = {
   title: 'Evaluation/ConfrontingViewpoints',
   component: ConfrontingViewpoint,
   tags: ['autodocs']
-} satisfies Meta<ConfrontingViewpoint>
+} satisfies Meta<typeof ConfrontingViewpoint>
 
 export default meta
 type Story = StoryObj<typeof meta>
@@ -17,23 +18,17 @@ export const Default: Story = {
       {
         id: 1,
         choice: 2,
-        explanation: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in quam dignissim, mattis diam in, hendrerit nisl. Suspendisse potenti. Aenean massa lorem, placerat ut nibh nec, iaculis rutrum orci. Quisque et sollicitudin elit. Ut ornare ante quis mauris scelerisque pretium. Nunc pellentesque nec velit et tincidunt. Aliquam erat volutpat. Aenean justo ex, dignissim vulputate orci auctor, consectetur sagittis lacus.
-
-`
-      },
+        explanation: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In in quam dignissim, mattis diam in, hendrerit nisl. Suspendisse potenti. Aenean massa lorem, placerat ut nibh nec, iaculis rutrum orci. Quisque et sollicitudin elit. Ut ornare ante quis mauris scelerisque pretium. Nunc pellentesque nec velit et tincidunt. Aliquam erat volutpat. Aenean justo ex, dignissim vulputate orci auctor, consectetur sagittis lacus.`
+      } as OpenEndedResponse,
       {
         id: 2,
         choices: [3, 4],
-        explanation: `In iaculis eros et tortor malesuada, aliquet bibendum neque sollicitudin. Mauris sodales turpis finibus ante tempor semper. Nam quis ligula nec ligula iaculis egestas. Aliquam vestibulum finibus elit. Duis rhoncus orci convallis, auctor mauris non, dapibus nulla. Duis id ligula metus. Suspendisse posuere finibus nulla.
-
-`
-      },
+        explanation: `In iaculis eros et tortor malesuada, aliquet bibendum neque sollicitudin. Mauris sodales turpis finibus ante tempor semper. Nam quis ligula nec ligula iaculis egestas. Aliquam vestibulum finibus elit. Duis rhoncus orci convallis, auctor mauris non, dapibus nulla. Duis id ligula metus. Suspendisse posuere finibus nulla.`
+      } as MultipleChoiceResponse,
       {
         id: 3,
-        explanation: `Vivamus elit felis, tempor non tellus a, laoreet mattis lacus. Sed aliquam mauris ut sem vulputate, sit amet semper tellus volutpat. Proin accumsan erat at elit interdum accumsan. Quisque porta, lacus vitae varius facilisis, tellus ipsum auctor felis, quis ornare nisl lacus non diam. Vivamus dapibus lacus sapien. Ut fermentum lobortis diam, eu feugiat metus pulvinar ac. Quisque venenatis tincidunt tempus. Suspendisse sollicitudin nisi quis leo molestie facilisis.
-
-`
-      }
+        explanation: `Vivamus elit felis, tempor non tellus a, laoreet mattis lacus. Sed aliquam mauris ut sem vulputate, sit amet semper tellus volutpat. Proin accumsan erat at elit interdum accumsan. Quisque porta, lacus vitae varius facilisis, tellus ipsum auctor felis, quis ornare nisl lacus non diam. Vivamus dapibus lacus sapien. Ut fermentum lobortis diam, eu feugiat metus pulvinar ac. Quisque venenatis tincidunt tempus. Suspendisse sollicitudin nisi quis leo molestie facilisis.`
+      } as ExclusiveChoiceResponse
     ]
   }
 }
