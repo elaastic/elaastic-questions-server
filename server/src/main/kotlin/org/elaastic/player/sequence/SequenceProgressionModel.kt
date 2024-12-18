ee91f0cfd40d84635b3e9305755f1b33111574ee
François SP <francois-sp@gmx.fr>
2024-12-18 17:04:37 +0100

    chore: clean up imports (remove unused on)

--- server/src/main/kotlin/org/elaastic/player/sequence/SequenceProgressionModel.kt
+++ server/src/main/kotlin/org/elaastic/player/sequence/SequenceProgressionModel.kt
@@ -1,14 +1,13 @@
 package org.elaastic.player.sequence
 
 import org.elaastic.player.command.CommandModel
-import org.elaastic.player.results.ResultsModel
 import org.elaastic.player.sequence.status.SequenceInfoModel
 import org.elaastic.player.steps.SequenceStatistics
 import org.elaastic.player.steps.StepsModel
 
 /**
- * Contain the data about the progression and the status of a sequence.
- * It's used for learners as they only need this information.
+ * Contain the data about the progression and the status of a sequence. It's used for learners as they only need this
+ * information.
  *
  * @param stepsModel The model containing the progression of the sequence.
  * @param sequenceInfoModel The model containing the status of the sequence.
@@ -19,8 +18,8 @@ open class SequenceProgressionModel(
 )
 
 /**
- * Contain the data about the progression, the status, the stats of a sequence and the command to manage it.
- * It's used for teachers as they need, in addition to see the progression, to manage the sequence.
+ * Contain the data about the progression, the status, the stats of a sequence and the command to manage it. It's used
+ * for teachers as they need, in addition to see the progression, to manage the sequence.
  *
  * @param stepsModel The model containing the progression of the sequence.
  * @param sequenceStatistics The statistics of the sequence.
