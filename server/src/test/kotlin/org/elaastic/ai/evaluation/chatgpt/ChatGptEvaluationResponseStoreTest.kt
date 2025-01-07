c2d2d065dce7662c8804a4c6602bcfea22b80246
François SP <francois-sp@gmx.fr>
2025-01-07 09:25:48 +0100

    test: add more test to `ChatGPTEvaluationResponseStore`

--- server/src/test/kotlin/org/elaastic/ai/evaluation/chatgpt/ChatGptEvaluationResponseStoreTest.kt
+++ server/src/test/kotlin/org/elaastic/ai/evaluation/chatgpt/ChatGptEvaluationResponseStoreTest.kt
@@ -53,4 +53,35 @@ class ChatGptEvaluationResponseStoreTest {
         // Then the two ChatGptEvaluationResponseStore should be equal
         assertEquals(chatGptEvaluationResponseStore1, chatGptEvaluationResponseStore2)
     }
+
+    @Test
+    fun `test equal with invalid store`() {
+        // Given two ChatGptEvaluationResponseStore with the same response ids
+        val chatGptEvaluationResponseStore1 = ChatGptEvaluationResponseStore()
+        chatGptEvaluationResponseStore1.addResponseIds(listOf(1, 2, 3))
+
+        // Then the two ChatGptEvaluationResponseStore should be equal
+        assertNotEquals(chatGptEvaluationResponseStore1, null)
+        assertNotEquals(chatGptEvaluationResponseStore1, "null")
+    }
+
+    @Test
+    fun `test responseHasBeenEvaluatedByChatGpt with invalid response id`() {
+        // Given a ChatGptEvaluationResponseStore with a response id
+        val chatGptEvaluationResponseStore = ChatGptEvaluationResponseStore(listOf(1))
+
+        // Then the response id should be stored
+        assertFalse(chatGptEvaluationResponseStore.responseHasBeenEvaluatedByChatGpt(null))
+    }
+
+    @Test
+    fun hashCodeTest() {
+        // Given two ChatGptEvaluationResponseStore with the same response ids
+        val chatGptEvaluationResponseStore1 = ChatGptEvaluationResponseStore()
+        chatGptEvaluationResponseStore1.addResponseIds(listOf(1, 2, 3))
+        val chatGptEvaluationResponseStore2 = ChatGptEvaluationResponseStore(listOf(1, 2, 3))
+
+        // Then the two ChatGptEvaluationResponseStore should have the same hash code
+        assertEquals(chatGptEvaluationResponseStore1.hashCode(), chatGptEvaluationResponseStore2.hashCode())
+    }
 }
