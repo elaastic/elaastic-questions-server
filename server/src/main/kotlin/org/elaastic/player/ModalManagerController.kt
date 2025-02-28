8d281b5b88eafb035c567a581d34e29cfe0837ed
François SP <francois-sp@gmx.fr>
2025-02-28 11:11:09 +0100

    feat: add report DRAXO modal to the manager.
    - Create new Controller that return modal
    - Implement ModalManagerController for report modals
    - Add functionality to fetch and display DRAXO evaluation reports
    - Update HTML templates to include report button and modal logic

---
+++ server/src/main/kotlin/org/elaastic/player/ModalManagerController.kt
@@ -0,0 +1,66 @@
+package org.elaastic.player
+
+import org.elaastic.activity.evaluation.peergrading.PeerGradingService
+import org.elaastic.ai.evaluation.chatgpt.ChatGptEvaluationService
+import org.elaastic.questions.assignment.sequence.peergrading.draxo.DraxoPeerGradingService
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/modal-manager")
+class ModalManagerController(
+    private val draxoPeerGradingService: DraxoPeerGradingService,
+    private val chatGptEvaluationService: ChatGptEvaluationService
+) {
+
+    /**
+     * Get the report modal for a DRAXO evaluation
+     *
+     * @param authentication the current user authentication
+     * @param model the model to add the report content
+     * @param draxoEvaluationId the id of the DRAXO evaluation
+     * @return the report modal
+     */
+    @GetMapping("/report/draxo/{draxoEvaluationId}")
+    fun reportDRAXOEvaluation(
+        authentication: Authentication,
+        model: Model,
+        @PathVariable draxoEvaluationId: Long,
+    ): String {
+        val draxoPeerGrading = draxoPeerGradingService.getDraxoPeerGrading(draxoEvaluationId)
+
+        model["reportContent"] = draxoPeerGrading.annotation!!
+        model["evaluationId"] = draxoPeerGrading.id!!
+        model["responseId"] = draxoPeerGrading.response.id!!
+        return "player/assignment/sequence/components/peer-grading-reaction/_peer-grading-draxo-reaction-report-modal.html :: reportModal"
+    }
+
+
+    /**
+     * Get the report modal for a ChatGPT evaluation
+     *
+     * @param authentication the current user authentication
+     * @param model the model to add the report content
+     * @param iaEvaluationId the id of the ChatGPT evaluation
+     * @return the report modal
+     */
+    @GetMapping("/report/ia/{iaEvaluationId}")
+    fun reportIAEvaluation(
+        authentication: Authentication,
+        model: Model,
+        @PathVariable iaEvaluationId: Long,
+    ): String {
+        val iaEvaluation = chatGptEvaluationService.findEvaluationById(iaEvaluationId)!!
+
+        model["sequenceId"] = iaEvaluation.response.interaction.sequence.id!!
+        model["reportContent"] = iaEvaluation.annotation!!
+        model["reportReasons"] = iaEvaluation.reportReasons!!
+        model["evaluationId"] = iaEvaluation.id!!
+        return "player/assignment/sequence/components/chat-gpt-evaluation/_chat-gpt-evaluation-report-modal.html :: reportModal"
+    }
+}
