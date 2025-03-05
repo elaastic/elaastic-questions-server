ae34b00f3188d83f29e163ccefbb1e84044efdd7
François SP <francois-sp@gmx.fr>
2025-03-05 14:37:49 +0100

    style: reformat file + add bracket to if statement

--- server/src/main/kotlin/org/elaastic/analytics/lrs/EventLogService.kt
+++ server/src/main/kotlin/org/elaastic/analytics/lrs/EventLogService.kt
@@ -11,57 +11,59 @@ import javax.transaction.Transactional
 @Service
 @Transactional
 class EventLogService(
-        @Autowired val eventLogRepository: EventLogRepository
+    @Autowired val eventLogRepository: EventLogRepository
 ) {
 
-    fun create(sequence: Sequence,
-               user: User,
-               action: Action,
-               obj: ObjectOfAction,
-               userAgent : String? = null): EventLog =
-            EventLog(
-                sequence = sequence,
-                user = user,
-                role = if (user != sequence.owner) Role.RoleId.STUDENT else Role.RoleId.TEACHER,
-                action = action,
-                obj = obj,
-                userAgent = userAgent
-            ).let(eventLogRepository::save)
+    fun create(
+        sequence: Sequence,
+        user: User,
+        action: Action,
+        obj: ObjectOfAction,
+        userAgent: String? = null
+    ): EventLog =
+        EventLog(
+            sequence = sequence,
+            user = user,
+            role = if (user != sequence.owner) Role.RoleId.STUDENT else Role.RoleId.TEACHER,
+            action = action,
+            obj = obj,
+            userAgent = userAgent
+        ).let(eventLogRepository::save)
 
     fun findById(id: Long): EventLog =
-            eventLogRepository.findById(id).get()
+        eventLogRepository.findById(id).get()
 
-    fun save(eventLog: EventLog){
+    fun save(eventLog: EventLog) {
         eventLogRepository.save(eventLog)
     }
 
-    fun saveActionsAfterClosingConfigurePopup(sequence : Sequence){
+    fun saveActionsAfterClosingConfigurePopup(sequence: Sequence) {
         create(sequence, sequence.owner, Action.CLOSE, ObjectOfAction.CONFIGURE_POPUP)
         create(sequence, sequence.owner, Action.START, ObjectOfAction.SEQUENCE)
         create(sequence, sequence.owner, Action.START, ObjectOfAction.PHASE_1)
     }
 
-    fun stopSequence(sequence : Sequence){
+    fun stopSequence(sequence: Sequence) {
         create(sequence, sequence.owner, Action.STOP, ObjectOfAction.SEQUENCE)
     }
 
-    fun reopenSequence(sequence : Sequence){
+    fun reopenSequence(sequence: Sequence) {
         create(sequence, sequence.owner, Action.RESTART, ObjectOfAction.SEQUENCE)
     }
 
-    fun startPhase(sequence : Sequence, phaseNumber: Int){
+    fun startPhase(sequence: Sequence, phaseNumber: Int) {
         create(sequence, sequence.owner, Action.START, ObjectOfAction.from("phase_$phaseNumber"))
     }
 
-    fun stopPhase(sequence : Sequence, phaseNumber: Int){
+    fun stopPhase(sequence: Sequence, phaseNumber: Int) {
         create(sequence, sequence.owner, Action.STOP, ObjectOfAction.from("phase_$phaseNumber"))
     }
 
-    fun skipPhase(sequence : Sequence, phaseNumber: Int){
+    fun skipPhase(sequence: Sequence, phaseNumber: Int) {
         create(sequence, sequence.owner, Action.SKIP, ObjectOfAction.from("phase_$phaseNumber"))
     }
 
-    fun restartPhase(sequence : Sequence, phaseNumber: Int){
+    fun restartPhase(sequence: Sequence, phaseNumber: Int) {
         create(sequence, sequence.owner, Action.RESTART, ObjectOfAction.from("phase_$phaseNumber"))
     }
 
@@ -82,11 +84,12 @@ class EventLogService(
     }
 
     fun consultPlayer(sequence: Sequence, user: User, learnerSequence: ILearnerSequence, userAgent: String?) {
-        if (learnerSequence.sequence.resultsArePublished)
-            if (learnerSequence.sequence.executionIsFaceToFace())
+        if (learnerSequence.sequence.resultsArePublished) {
+            if (learnerSequence.sequence.executionIsFaceToFace()) {
                 consultResults(sequence, user, userAgent)
-            else
-                if (learnerSequence.activeInteraction?.isRead() == true)
-                    consultResults(sequence, user, userAgent)
+            } else if (learnerSequence.activeInteraction?.isRead() == true) {
+                consultResults(sequence, user, userAgent)
+            }
+        }
     }
 }
