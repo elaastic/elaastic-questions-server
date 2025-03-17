99e82ddd6410a11e353aa64ed67925ff60b990fe
John Tranier <john.tranier@isae-supaero.fr>
2025-03-17 12:00:31 +0100

    feat: Add OIDC logout handler in addition to existing logout handler

--- server/src/main/kotlin/org/elaastic/auth/cas/ElaasticUrlLogoutSuccessHandler.kt
+++ server/src/main/kotlin/org/elaastic/auth/cas/ElaasticUrlLogoutSuccessHandler.kt
@@ -1,6 +1,24 @@
+/*
+ * Elaastic - formative assessment system
+ * Copyright (C) 2019. University Toulouse 1 Capitole, University Toulouse 3 Paul Sabatier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.elaastic.auth.cas
 
-import org.elaastic.user.User
+import org.elaastic.user.PrincipalUserResolver
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler
@@ -13,7 +31,7 @@ typealias Url = String
 /**
  * This LogoutSuccessHandler manage logouts from the elaastic formLogin and the configured CAS servers as well
  * If the session is not bound to a CAS session, it will redirect to formLogoutSuccessUrl
- * IF the session is bound to a CAS session, it will redirect to the CAS server logout URL
+ * If the session is bound to a CAS session, it will redirect to the CAS server logout URL
  */
 class ElaasticUrlLogoutSuccessHandler(
     formLogoutSuccessUrl: String,
@@ -25,17 +43,18 @@ class ElaasticUrlLogoutSuccessHandler(
         this.defaultTargetUrl = formLogoutSuccessUrl
     }
 
+
     override fun determineTargetUrl(
         request: HttpServletRequest,
         response: HttpServletResponse,
         authentication: Authentication?
     ): String {
-        val user = authentication?.principal as User?
+        val user = (authentication?.principal as PrincipalUserResolver?)?.elaasticUser
 
         return if (user?.casKey != null) {
             (casKeyToServerUrl[user.casKey]
                 ?: throw IllegalStateException("No logout URL configured for the CAS server [${user.casKey}]")) +
-                        casLogoutSuccessUrl
+                    casLogoutSuccessUrl
         } else super.determineTargetUrl(request, response, authentication)
     }
 }
