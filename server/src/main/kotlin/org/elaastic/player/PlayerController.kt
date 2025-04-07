f9dc74e375024d746c3bca0c6717c4468ca81d8d
John Tranier <tranier@gmail.com>
2025-04-07 18:56:00 +0200

    fix: Merge errors on PlayerController

--- server/src/main/kotlin/org/elaastic/player/PlayerController.kt
+++ server/src/main/kotlin/org/elaastic/player/PlayerController.kt
@@ -156,9 +156,9 @@ class PlayerController(
         authentication: Authentication,
         @RequestParam("globalId") globalId: String
     ): String {
-        return doRegister(
-            authentication.principal as User,
-            findAssignment(globalId)
+        val user = (authentication.principal as PrincipalUserResolver).elaasticUser
+
+        return doRegister(user, findAssignment(globalId)
         )
     }
 
@@ -301,7 +301,7 @@ class PlayerController(
         model: Model,
         @PathVariable sequenceId: Long
     ): String {
-        val user: User = authentication.principal as User
+        val user = (authentication.principal as PrincipalUserResolver).elaasticUser
         val sequence = sequenceService.get(sequenceId, true)
         val isTeacher = user == sequence.owner
 
@@ -637,8 +637,6 @@ class PlayerController(
         model: Model,
         @PathVariable responseId: Long
     ): String {
-        authentication.principal as User
-
         val response = responseService.findById(responseId)
         val chatGptEvaluation = chatGptEvaluationService.findEvaluationByResponse(response)
         model.addAttribute(
