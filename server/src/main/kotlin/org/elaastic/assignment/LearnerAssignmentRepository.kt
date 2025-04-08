7f8dbd8fbae43b8591be427c709b0da28bbe7d8c
François SP <francois-sp@gmx.fr>
2025-04-08 12:22:24 +0200

    chore!: Rename CasUser to UserLink
    - add migration script

--- server/src/main/kotlin/org/elaastic/assignment/LearnerAssignmentRepository.kt
+++ server/src/main/kotlin/org/elaastic/assignment/LearnerAssignmentRepository.kt
@@ -16,6 +16,6 @@ interface LearnerAssignmentRepository : JpaRepository<LearnerAssignment, Long> {
     // TODO: order by phase & name.
     fun findAllByAssignment(assignment: Assignment): List<LearnerAssignment>
 
-    @Query("SELECT DISTINCT u from User u JOIN u.registrations la LEFT JOIN FETCH u.casUser WHERE la.assignment = ?1")
+    @Query("SELECT DISTINCT u from User u JOIN u.registrations la LEFT JOIN FETCH u.userLink WHERE la.assignment = ?1")
     fun findAllRegisteredLearnersOnAssignmentWithCasUser(assignment: Assignment): List<User>
 }
