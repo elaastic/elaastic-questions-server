2f6838c3cee3836d23e7fb4a637241a2aca792eb
François SP <francois-sp@gmx.fr>
2025-04-09 16:15:05 +0200

    fix: add missing copyright

--- server/src/test/kotlin/org/elaastic/assignment/AssignmentServiceIntegrationTest.kt
+++ server/src/test/kotlin/org/elaastic/assignment/AssignmentServiceIntegrationTest.kt
@@ -1,3 +1,21 @@
+/*
+ * Elaastic - formative assessment system
+ * Copyright (C) 2019. University Toulouse 1 Capitole, University Toulouse 3 Paul Sabatier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package org.elaastic.assignment
 
 import org.elaastic.activity.response.ConfidenceDegree
@@ -94,7 +112,7 @@ internal class AssignmentServiceIntegrationTest(
         (1..n).forEach {
             assignmentService.save(
                 Assignment(
-                    title = "Assignment n°$it",
+                    title = "Assignment nï¿½$it",
                     owner = owner
                 )
             )
@@ -378,7 +396,7 @@ internal class AssignmentServiceIntegrationTest(
             subjectService.addStatement(
                 subject,
                 Statement.createDefaultStatement(subject.owner)
-                    .title("Sequence n°1")
+                    .title("Sequence nï¿½1")
                     .content("Content 1")
             )
         }.tThen {
@@ -401,7 +419,7 @@ internal class AssignmentServiceIntegrationTest(
         val statement1 = subjectService.addStatement(
             subject,
             Statement.createDefaultStatement(subject.owner)
-                .title("Sequence n°1")
+                .title("Sequence nï¿½1")
                 .content("Content 1")
         )
         val assignment = subjectService.addAssignment(
