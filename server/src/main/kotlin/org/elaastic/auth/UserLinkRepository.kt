7cec327dfce376cc21a35e2847b99458b50c2a6f
François SP <francois-sp@gmx.fr>
2025-04-11 16:57:15 +0200

    feat: block link user to change their password
    Add methods to check if a user is linked to an external provider.
    Prevent password changes for users linked to external providers.
    Include integration tests for the new UserLink repository methods.
    Enhance documentation for methods to improve clarity.

--- server/src/main/kotlin/org/elaastic/auth/UserLinkRepository.kt
+++ server/src/main/kotlin/org/elaastic/auth/UserLinkRepository.kt
@@ -1,5 +1,6 @@
 package org.elaastic.auth
 
+import org.elaastic.user.User
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.repository.CrudRepository
 
@@ -7,4 +8,6 @@ interface UserLinkRepository : CrudRepository<UserLink, Long> {
 
     @EntityGraph(value = "UserLink.user.roles", type = EntityGraph.EntityGraphType.LOAD)
     fun findByProviderIdAndProviderUserId(providerId: String, providerUserId: String): UserLink?
+
+    fun findByUser(user: User): UserLink?
 }
