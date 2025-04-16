b4d7e0977c7c87dc8055521fba2aaa1570eb9489
François SP <francois-sp@gmx.fr>
2025-04-16 11:08:06 +0200

    feat: enhance role extraction logic
    - Introduces constants for role claim keys
    - Improves validation to ensure a single valid role
    - Logs and filters roles consistently for better clarity
    - Throws exception for invalid or missing roles

--- server/src/main/kotlin/org/elaastic/auth/oauth/ElaasticOidcUserService.kt
+++ server/src/main/kotlin/org/elaastic/auth/oauth/ElaasticOidcUserService.kt
@@ -19,6 +19,7 @@ package org.elaastic.auth.oauth
 
 import org.elaastic.auth.UserLinkService
 import org.elaastic.user.Role
+import org.elaastic.user.Role.RoleId
 import org.elaastic.user.contains
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -26,6 +27,16 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.stereotype.Service
+
+/**
+ * Key to get the realm access from the OIDC user's claims
+ */
+private const val REALM_ACCESS_KEY = "realm_access"
+/**
+ * Key to get the roles from the realm access
+ * @see REALM_ACCESS_KEY
+ */
+private const val ROLES_KEY = "roles"
 
 /**
  * Service dedicated to retrieve the Elaastic User bound to an OidcUser
@@ -61,18 +72,53 @@ class ElaasticOidcUserService(
         return ElaasticOidcUser(oidcUser, user)
     }
 
-    private fun getRoleFromOidcUser(oidcUser: OidcUser): Role.RoleId {
-        val claimAsMap = oidcUser.getClaimAsMap("realm_access")
-        logger.info(claimAsMap.toString())
-        val realmRoles: List<String> = (claimAsMap["roles"] as List<*>)
+    /**
+     * Get the role from the OIDC user
+     *
+     * We search the role in the realm access of the OIDC user.
+     * The realm access is a map containing the roles of the user.
+     *
+     * We expected a claims like this :
+     *
+     * ```json
+     * "claims": {
+     *     [...]
+     *     "realm_access": {
+     *         [...]
+     *         "roles": [
+     *             "student"
+     *         ]
+     *     }
+     * }
+     * ```
+     * An [IllegalStateException] is thrown if:
+     * - there is more than one role in the realm access
+     * - there is no role in the realm access
+     * - the role is not one of the following: "admin", "teacher", "student"
+     *
+     * @throws IllegalStateException if there is not exactly one role in the realm access and the role is unknown
+     * @param oidcUser the OIDC user
+     * @return the role of the OIDC user
+     * @see REALM_ACCESS_KEY
+     * @see ROLES_KEY
+     */
+    private fun getRoleFromOidcUser(oidcUser: OidcUser): RoleId {
+        val realmRoles: List<String> = (oidcUser.getClaimAsMap(REALM_ACCESS_KEY)[ROLES_KEY] as List<*>)
             .also { logger.info(it.toString()) }
             .filterIsInstance<String>()
             .map { it.lowercase() }
 
+        check(realmRoles.size <= 1) {
+            "There should be only one role in the realm roles: $realmRoles"
+        }
+        check(realmRoles.isNotEmpty()) {
+            "There should be at least one role in the realm roles: $realmRoles"
+        }
+
         return when {
-            realmRoles.contains("admin") -> Role.RoleId.ADMIN
-            realmRoles.contains("teacher") -> Role.RoleId.TEACHER
-            realmRoles.contains("student") -> Role.RoleId.STUDENT
+            realmRoles.contains("admin") -> RoleId.ADMIN
+            realmRoles.contains("teacher") -> RoleId.TEACHER
+            realmRoles.contains("student") -> RoleId.STUDENT
             else -> throw IllegalStateException("No valid role found among realm roles: $realmRoles")
         }
     }
