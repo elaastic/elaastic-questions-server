23b00fb66432dca6dcf7b372ba3649fc0eb350f6
François SP <francois-sp@gmx.fr>
2025-04-17 14:24:05 +0200

    fix: small fix
    - Refines error path authorization to match all subdirectories with "/error/**" for better route matching
    - Clarifies wording in documentation regarding user roles and login permissions

--- server/src/main/kotlin/org/elaastic/security/WebSecurityConfig.kt
+++ server/src/main/kotlin/org/elaastic/security/WebSecurityConfig.kt
@@ -177,7 +177,7 @@ class WebSecurityConfig(
                 authorize("/ui/**", permitAll)
                 authorize("/register", permitAll)
                 authorize("/api/users", permitAll)
-                authorize("/error/*", permitAll)
+                authorize("/error/**", permitAll)
                 authorize(LOGIN_URL, permitAll)
 
                 // Allow access to this URL on which the CAS filter are applied
