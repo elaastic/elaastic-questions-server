7003e3f471ed27573987cada7450fd615fe0700b
François SP <francois-sp@gmx.fr>
2025-04-17 14:17:44 +0200

    fix: allow the OIDCUser to log out of his OIDC provider
    - Implement fake authentication for OIDC role errors for logout compatibility
    - Modify authentication failure handler to manage RoleException more accurately, utilizing the user request session attribute

--- server/src/main/kotlin/org/elaastic/auth/oauth/RoleExceptionController.kt
+++ server/src/main/kotlin/org/elaastic/auth/oauth/RoleExceptionController.kt
@@ -1,25 +1,73 @@
 package org.elaastic.auth.oauth
 
+import org.elaastic.user.User
 import org.springframework.context.MessageSource
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.context.SecurityContextImpl
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
+import org.springframework.security.oauth2.client.registration.ClientRegistration
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.SessionAttribute
 import java.util.*
+
+const val USER_REQUEST_ATTRIBUTE = "userRequest"
+const val ERROR_ROLE_URL = "/error/oidc_role"
 
 @Controller
 class RoleExceptionController(
     private val messageSource: MessageSource
 ) {
 
-    @GetMapping("error/oidc_role")
+    @GetMapping(ERROR_ROLE_URL)
     fun handleRoleException(
         model: Model,
         @RequestParam("message", required = false) message: String?,
-        locale: Locale
+        locale: Locale,
+        @SessionAttribute(name = USER_REQUEST_ATTRIBUTE, required = true) oidcUserRequest: OidcUserRequest
     ): String {
+        /**
+         * For the logout process to work, we need a valid authentication. So we create a fake authentication with the
+         * OIDC user
+         *
+         * @see fakeAuthenticationFrom
+         */
+        SecurityContextHolder.setContext(SecurityContextImpl(fakeAuthenticationFrom(oidcUserRequest)))
+
         model["errorMessage"] = message ?: messageSource.getMessage("oidc.role_error", null, locale)
         return "error/oidc_role"
+    }
+
+    /**
+     * Create a fake authentication from the OidcUserRequest
+     *
+     * As we only care for the [OidcUserRequest.idToken] and the [ClientRegistration.registrationId], we set all the
+     * other information to default values.
+     */
+    private fun fakeAuthenticationFrom(
+        oidcUserRequest: OidcUserRequest
+    ): OAuth2AuthenticationToken {
+        return OAuth2AuthenticationToken(
+            ElaasticOidcUser(
+                DefaultOidcUser(
+                    emptyList(),
+                    oidcUserRequest.idToken
+                ),
+                // We don't care about the information on the user
+                User(
+                    firstName = "FirstName",
+                    lastName = "LastName",
+                    username = "Username",
+                    plainTextPassword = "Password",
+                )
+            ),
+            emptyList(),
+            oidcUserRequest.clientRegistration.registrationId
+        )
     }
 }
