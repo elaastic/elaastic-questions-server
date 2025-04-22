f0df6a9d1614b542ca27db6ad5e4305ce3bd7815
François SP <francois-sp@gmx.fr>
2025-04-22 16:50:44 +0200

    feat: Add OIDC login success handler
    - Introduce OidcLoginSuccessHandler to handle successful OIDC logins
    - Redirect users based on session attribute or default to root
    - Integrate OidcLoginSuccessHandler into WebSecurityConfig

--- server/src/main/kotlin/org/elaastic/security/WebSecurityConfig.kt
+++ server/src/main/kotlin/org/elaastic/security/WebSecurityConfig.kt
@@ -22,6 +22,7 @@ import org.elaastic.auth.ElaasticLogoutSuccessHandler
 import org.elaastic.auth.cas.ElaasticUrlLogoutSuccessHandler
 import org.elaastic.auth.oauth.ElaasticOidcUserService
 import org.elaastic.auth.oauth.OidcHintFilter
+import org.elaastic.auth.oauth.OidcLoginSuccessHandler
 import org.elaastic.user.Role
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -61,6 +62,7 @@ class WebSecurityConfig(
     @Autowired val encoder: PasswordEncoder,
     @Autowired val elaasticOidcUserService: ElaasticOidcUserService,
     @Autowired val clientRegistrationRepository: ClientRegistrationRepository,
+    private val oidcLoginSuccessHandler: OidcLoginSuccessHandler,
     @Value("\${elaastic.questions.url}") val elaasticUrl: String,
     @Value("\${elaastic.openid.enabled:false}") val elaasticOidcEnabled: Boolean,
 ) {
@@ -111,6 +113,7 @@ class WebSecurityConfig(
                     userInfoEndpoint {
                         oidcUserService = elaasticOidcUserService
                     }
+                    authenticationSuccessHandler = oidcLoginSuccessHandler
                 }
                 http.addFilterBefore(
                     OidcHintFilter(),
