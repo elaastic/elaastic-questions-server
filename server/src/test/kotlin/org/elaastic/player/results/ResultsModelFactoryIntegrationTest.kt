15974c39dfa97259813101937fcbac7ea2fe24a7
François SP <francois-sp@gmx.fr>
2025-04-23 17:21:51 +0200

    feat: add response set creation and integration tests
    - Implement functionality to create ResponseSet with user-generated responses
    - Refactor existing tests and add new integration tests for response set creation
    - Enhance error handling in UserService with improved role validation message
    - Introduce ResultsModelFactoryIntegrationTest for testing result models
    
    Relates to issue #123

---
+++ server/src/test/kotlin/org/elaastic/player/results/ResultsModelFactoryIntegrationTest.kt
@@ -0,0 +1,58 @@
+package org.elaastic.player.results
+
+import org.elaastic.ai.evaluation.chatgpt.ChatGptEvaluationResponseStore
+import org.elaastic.common.web.MessageBuilder
+import org.elaastic.material.instructional.question.QuestionType
+import org.elaastic.sequence.State
+import org.elaastic.sequence.config.ResponseSubmissionSpecification
+import org.elaastic.sequence.interaction.Interaction
+import org.elaastic.sequence.interaction.InteractionRepository
+import org.elaastic.sequence.interaction.InteractionType
+import org.elaastic.test.FunctionalTestingService
+import org.elaastic.test.IntegrationTestingService
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class ResultsModelFactoryIntegrationTest(
+    @Autowired val integrationTestingService: IntegrationTestingService,
+    @Autowired val functionalTestingService: FunctionalTestingService,
+    @Autowired val messageBuilder: MessageBuilder,
+    @Autowired val interactionRepository: InteractionRepository,
+) {
+
+    @Test
+    fun `test build`() {
+        val sequence = integrationTestingService.getAnySequence()
+            .also {
+                it.statement.questionType = QuestionType.MultipleChoice
+                val interaction = Interaction(
+                    interactionType = InteractionType.ResponseSubmission,
+                    rank = 1,
+                    owner = it.owner,
+                    sequence = it,
+                    state = State.show,
+                    specification = ResponseSubmissionSpecification(true, true)
+                ).also(interactionRepository::save)
+                it.interactions[InteractionType.ResponseSubmission] = interaction
+                it.activeInteraction = interaction
+            }
+
+        val responseSet = functionalTestingService.createResponseSet(sequence, 2)
+
+        val resultsModel = ResultsModelFactory.build(
+            teacher = true,
+            sequence = sequence,
+            responseSet = responseSet,
+            userCanRefreshResults = true,
+            messageBuilder = messageBuilder,
+            chatGptEvaluationResponseStore = ChatGptEvaluationResponseStore()
+        )
+
+        assertNotNull(resultsModel)
+        assertInstanceOf(ChoiceResultsModel::class.java, resultsModel)
+    }
+}
