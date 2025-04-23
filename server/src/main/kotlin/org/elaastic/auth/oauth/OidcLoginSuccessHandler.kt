b372b22ef8e249614fde4360bf935451f22f2f0e
François SP <francois-sp@gmx.fr>
2025-04-23 14:24:10 +0200

    fix: update redirect logic on login success
    - use Spring security saved request as a fallback and after that '/home'

--- server/src/main/kotlin/org/elaastic/auth/oauth/OidcLoginSuccessHandler.kt
+++ server/src/main/kotlin/org/elaastic/auth/oauth/OidcLoginSuccessHandler.kt
@@ -3,6 +3,7 @@ package org.elaastic.auth.oauth
 import org.elaastic.auth.oauth.OidcHintFilter.Companion.TARGET_URL_SESSION_ATTR
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.security.web.savedrequest.SavedRequest
 import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -15,7 +16,9 @@ class OidcLoginSuccessHandler : AuthenticationSuccessHandler {
         response: HttpServletResponse,
         authentication: Authentication
     ) {
-        val targetUrl = request.session.getAttribute(TARGET_URL_SESSION_ATTR) as? String ?: "/"
+        val targetUrl = request.session.getAttribute(TARGET_URL_SESSION_ATTR) as? String
+            ?: (request.session.getAttribute("SPRING_SECURITY_SAVED_REQUEST") as? SavedRequest)?.redirectUrl
+            ?: "/home"
 
         request.session.removeAttribute(TARGET_URL_SESSION_ATTR)
 
