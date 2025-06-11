b0b4f3a17b1a98680ee4cf906e22845670b2233c
François SP <francois.sp@ik.me>
2025-06-11 16:49:02 +0200

    feat: add test page to integrate SequenceConfiguration component

---
+++ server/src/main/kotlin/org/elaastic/sequence/SequenceUiController.kt
@@ -0,0 +1,13 @@
+package org.elaastic.sequence
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("ui/sequence")
+class SequenceUiController {
+
+    @GetMapping("/sequence-configuration")
+    fun sequenceConfiguration() = "player/assignment/sequence/components/command/sequence-config"
+}
