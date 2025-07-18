eda9c62c55a6dace529c99c5e71fc354286e3338
alexLcmt <alexlecomte144@gmail.com>
2025-07-18 16:42:44 +0200

    update: create an interface for the props

--- ui-components/src/components/sequence/ListSequenceOverview.vue
+++ ui-components/src/components/sequence/ListSequenceOverview.vue
@@ -1,21 +1,23 @@
-<script setup lang="ts">
-
-import {type PropType, ref} from "vue";
-import SequenceOverview from "@/components/sequence/SequenceOverview.vue"
+<script lang="ts" xmlns:slot="http://www.w3.org/1999/html">
 import type {Sequence} from "@/components/sequence/Sequence.types";
 
-const props = defineProps({
+export interface ListSequenceOverviewProps {
   /**
    * All the sequences of a subject. It's an array of Object composed of 3 attributes : the state of the sequence so it can be 'NOT_STARTED' | 'CLOSED' | 'IN_PROGRESS' . The second attribute is a 2-uplet : the title and the statement of the question. The third attribute is the id of the sequence.
    */
-  sequences: {
-    type: Array as PropType<Sequence[]>
-  },
-})
+  sequences: Sequence[]
+}
+</script>
+<script setup lang="ts">
+
+import {ref} from "vue";
+import SequenceOverview from "@/components/sequence/SequenceOverview.vue"
+
+const props = defineProps<ListSequenceOverviewProps>()
 
 const emits = defineEmits(["changeSelectedQuestion"])
 
-const selectedSequenceIndex = ref<number | null>(null)
+const selectedSequenceIndex = ref<number>(0)
 
 const handleSelectedQuestion = (newValue: number) => {
   selectedSequenceIndex.value = newValue
