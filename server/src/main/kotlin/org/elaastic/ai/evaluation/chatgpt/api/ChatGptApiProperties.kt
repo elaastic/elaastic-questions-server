37acadb276c5e2168db2242c7078e5c8f23fda90
John Tranier <john.tranier@isae-supaero.fr>
2025-10-29 10:14:25 +0100

    refactor(chatgpt): use ChatGptApiProperties only with "chatgpt" profile

--- server/src/main/kotlin/org/elaastic/ai/evaluation/chatgpt/api/ChatGptApiProperties.kt
+++ server/src/main/kotlin/org/elaastic/ai/evaluation/chatgpt/api/ChatGptApiProperties.kt
@@ -1,7 +1,9 @@
 package org.elaastic.ai.evaluation.chatgpt.api
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Profile
 
+@Profile("chatgpt")
 @ConfigurationProperties(prefix = "chatgptapi")
 class ChatGptApiProperties(
     val token: String,
